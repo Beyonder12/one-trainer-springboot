5e01d6666452bb63e3888d3d24918e2c94060fc6
Beyonder <fajri.illahi1211@gmail.com>
2025-12-05 11:32:12 +0700

    Desain Rest API: Create mock APIs

---
+++ product-service/src/main/java/com/beyonder/productservice/mockapi/ProductController.java
@@ -0,0 +1,33 @@
+package com.beyonder.productservice.mockapi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "Get Products";
+    }
+
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable Long id) {
+        return "Get Product by ID: " + id;
+    }
+
+    @PostMapping
+    public String createProduct(@RequestBody String body) {
+        return "Create Product with data: " + body;
+    }
+
+    @PutMapping("/{id}")
+    public String updateProduct(@PathVariable Long id, @RequestBody String body) {
+        return "Update Product with ID: " + id + ", data: " + body;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        return "Delete Product with ID: " + id;
+    }
+}
