02be46dc1dd9b0961d6e82b8be9eedbc80b43d6f
Beyonder <fajri.illahi1211@gmail.com>
2025-12-05 15:35:10 +0700

    API Gateway Vs App: Init spring cloud starter

---
+++ gateway-service/src/test/java/com/beyonder/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyonder.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
