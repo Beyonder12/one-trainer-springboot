18efcb309273b17b9860785d8c1c01c2792e0947
Fajri Illahi <fajri.illahi@ifg-life.id>
2025-12-07 11:57:54 +0700

    Update minor

--- book-service/src/main/java/com/beyonder/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/beyonder/bookservice/controller/BookController.java
@@ -5,13 +5,15 @@ import com.beyonder.bookservice.dto.BookRespDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/books")
 public class BookController {
 
-    @PostMapping("/books")
-    public ResponseEntity<BookRespDto> getBooks(@RequestBody BookReqDto bookReqDto) {
+    @PostMapping("/using-dummy-dto")
+    public ResponseEntity<BookRespDto> createBook(@RequestBody BookReqDto bookReqDto) {
         // Instantiate response book data (dto(data transfer object))
         BookRespDto bookRespDto = new BookRespDto();
 
